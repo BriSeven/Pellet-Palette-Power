dada79768dc3e765914ba770bee40945cbba8d1b
wendy <wendy@rory.(none)>
2012-01-27 01:43:12 +1100

    Added method MakeTestPhysicsCtx. Still really exactly the same as the old MakeTestCtx. A context by any other name is still the exact same context it was before...

--- load.lua
+++ load.lua
@@ -19,8 +19,8 @@ function love.load()
 	
 
 
-	ctx=makeTestCTX(love)
-	
+	--ctx=makeTestCTX(love)
+	ctx = makeTestPhysicsCTX(love)
 	--love.audio.play(music, 0)
 	pickmode()
 	
@@ -29,6 +29,41 @@ end
 
 
 function makeTestCTX (love)
+	local ctx= {}
+
+	ctx.joysticks = love.joystick.getNumJoysticks( )
+
+	function ctx.name (name, character) 
+		--replacable with proper cache lookup later
+		return ctx[name]
+	end
+	ctx.ph = Playhead.create()
+	ctx.love=love
+	-- The amazing music.
+	--music = love.audio.newSource("mav-alon.it")
+	-- The various images used.
+	ctx.body   	= love.graphics.newImage(db.name( "body"   	))
+	ctx.ear    	= love.graphics.newImage(db.name( "ear"    	))
+	ctx.face   	= love.graphics.newImage(db.name( "face"   	))
+	ctx.logo   	= love.graphics.newImage(db.name( "logo"   	))
+	ctx.cloud  	= love.graphics.newImage(db.name( "cloud"  	))
+	ctx.selfmag	= love.graphics.newImage(db.name( "selfmag"	))
+
+	ctx.nekochan = Nekochan.create()
+	print(ctx.nekochan)
+	-- Set the background color to soothing pink.
+	ctx.love.graphics.setBackgroundColor(0xff, 0xf1, 0xf7)
+	ctx.clouds=Clouds.create();
+		-- Spawn some clouds.
+	for i=1,5 do
+		ctx.clouds:spawn_cloud(math.random(-100, 900), math.random(-100, 700), 80 + math.random(0, 50))
+	end
+	ctx.love.graphics.setColor(255, 255, 255, 200)
+	return ctx
+
+end 
+
+function makeTestPhysicsCTX (love)
 	local ctx= {}
 
 	ctx.joysticks = love.joystick.getNumJoysticks( )
