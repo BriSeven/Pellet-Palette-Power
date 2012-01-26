cf44eefec3db0842622ee5ed82d3c711cfefe7cd
wendy <wendy@rory.(none)>
2012-01-27 03:07:08 +1100

    Added useful tool for printing out tables

---
+++ wendy_useful.lua
@@ -0,0 +1,21 @@
+-- See http://www.lua.org/pil/19.3.html
+
+function pairsByKeys (t, f)
+  local a = {}
+  for n in pairs(t) do table.insert(a, n) end
+  table.sort(a, f)
+  local i = 0      -- iterator variable
+  local iter = function ()   -- iterator function
+    i = i + 1
+    if a[i] == nil then return nil
+    else return a[i], t[a[i]]
+    end
+  end
+  return iter
+end
+
+function print_table(some_table)
+	for name, line in pairsByKeys(some_table) do
+	      print(name, line)
+	end
+end
